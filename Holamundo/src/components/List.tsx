e13584fc07bff6fed0576bfc03447f12ff7c2def
alvarog <alvarodimasjaen1234@gmail.com>
2026-01-22 12:01:41 +0100

    initial commit

---
+++ Holamundo/src/components/List.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react/jsx-runtime";
+
+function List() {
+  let items = ["Baza", "Granada", "España"];
+
+  return (
+    <div>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
